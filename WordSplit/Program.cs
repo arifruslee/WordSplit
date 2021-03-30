cf63e7395977aa48cd6fddafd83a1e8e915a30f3
Arif Faeetz Bin Ruslee <ariffaeetz.ruslee@grassvalley.com>
2021-03-30 09:54:08 +0800

    Add project files.

---
+++ WordSplit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordSplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
