6acd5abf6c41baf25bdd3ec904a982651a402dc9
Arif Faeetz Bin Ruslee <ariffaeetz.ruslee@grassvalley.com>
2021-03-31 10:54:51 +0800

    Code corrections: - Removed unnecessary conditions - Invert code to reduce nested ifs and improves readability

--- WordSplit/Program.cs
+++ WordSplit/Program.cs
@@ -13,29 +13,26 @@ namespace WordSplit
 
             var arraySecondElement = secondElement.Split(",");
 
-            if (arraySecondElement.Length > 0)
+            //Filter out to only those contained in firstElement
+            var listContainedElements = arraySecondElement.Where(w => firstElement.ToUpper().Contains(w.ToUpper())).ToList();
+
+            //Only continue process if more than 1 word exists
+            if (listContainedElements.Count <= 1)
             {
-                //Filter out to only those contained in firstElement
-                var listContainedElements = arraySecondElement.Where(w => firstElement.ToUpper().Contains(w.ToUpper())).ToList();
-
-                //Only process if more than 1 word exists
-                if (listContainedElements.Count > 1)
-                {
-                    //Check if combination of 2 words. Starts with and Ends with
-                    var firstWord = listContainedElements.Where(w => firstElement.ToUpper().StartsWith(w.ToUpper())).FirstOrDefault();
-                    var secondWord = listContainedElements.Where(w => firstElement.ToUpper().EndsWith(w.ToUpper())).FirstOrDefault();
-
-                    //Also checks to make sure there are no other additional chars. Only combination of both words.
-                    if (!String.IsNullOrEmpty(firstWord) && !String.IsNullOrEmpty(secondWord) && (firstWord + secondWord == firstElement))
-                        outputResult = firstWord + "," + secondWord;
-                    else
-                        outputResult = "String not possible";
-                }
-                else
-                    outputResult = "String not possible";
-            }
-            else
                 outputResult = "String not possible";
+                Console.WriteLine(outputResult);
+                return;
+            }
+
+            //Check if combination of 2 words. Starts with and Ends with
+            var firstWord = listContainedElements.Where(w => firstElement.ToUpper().StartsWith(w.ToUpper())).FirstOrDefault();
+            var secondWord = listContainedElements.Where(w => firstElement.ToUpper().EndsWith(w.ToUpper())).FirstOrDefault();
+
+            //Also checks to make sure there are no other additional chars. Only combination of both words.
+            if (firstWord + secondWord != firstElement)
+                outputResult = "String not possible";
+            else
+                outputResult = firstWord + "," + secondWord;
 
             Console.WriteLine(outputResult);
         }
